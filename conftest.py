f56cfe4a49c957584f5e379a237e4bbff898f1d9
aalekseev <alekseev.aa@lime-shop.com>
2025-04-24 10:52:39 +0700

    *

--- conftest.py
+++ conftest.py
@@ -23,7 +23,7 @@ def setup_browser(base_url):
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-notifications")
